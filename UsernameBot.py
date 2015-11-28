47f638723d5af4502df59c0aafe7fee41b9e67b0
lowky12 <lachlans49@gmail.com>
2015-11-28 11:16:05 +1100

    Cleaned up code and ran it through pylint. Version also now 1.2

--- UsernameBot.py
+++ UsernameBot.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python2
+"""
+Bot Core Functionality
+"""
 
 import praw
 import time
@@ -6,46 +9,70 @@ import settings
 from prawoauth2 import PrawOAuth2Server, PrawOAuth2Mini
 import UsernameManager
 
-user_agent = 'python:UsernameMod:v1.2 (by /u/lowky12)'
+USER_AGENT = 'python:UsernameMod:v1.2 (by /u/lowky12)'
 
-reddit_client = praw.Reddit(user_agent=user_agent)
-username_manager = UsernameManager.UsernameManager(reddit_client)
-already_done = set()
+REDDIT_CLIENT = praw.Reddit(user_agent=USER_AGENT)
+USERNAME_MANAGER = UsernameManager.UsernameManager(REDDIT_CLIENT)
+ALREADY_DONE = set()
 
 
 def bot_init():
-    oauthserver = PrawOAuth2Server(reddit_client, settings.reddit_app_key, settings.reddit_app_secret, state=user_agent,
-                                   scopes=settings.scopes)
-    oauthserver.start()
-    tokens = oauthserver.get_access_codes()
-    print(tokens)
+    """
+    Initialises the bot setting the refresh_token and the refresh_token through oauth
+    """
+    oauth_server = PrawOAuth2Server(REDDIT_CLIENT,
+                                    settings.reddit_app_key, settings.reddit_app_secret,
+                                    state=USER_AGENT, scopes=settings.scopes)
+    oauth_server.start()
+    tokens = oauth_server.get_access_codes()
+    print tokens
     write_config(tokens)
 
 
 def write_config(tokens):
-    settings = 'settings.py'
-    replacements = {'{{access_token}}': tokens['access_token'], '{{refresh_token}}': tokens['refresh_token']}
+    """
+    Writes the tokens to the settings file
+    :param tokens:
+    """
+    settings_file = 'settings.py'
+    replacements = {
+        '{{access_token}}': tokens['access_token'],
+        '{{refresh_token}}': tokens['refresh_token']
+    }
     lines = []
-    with open(settings) as infile:
+    with open(settings_file) as infile:
         for line in infile:
             for src, target in replacements.iteritems():
                 line = line.replace(src, target)
             lines.append(line)
-    with open(settings, 'w') as outfile:
+    with open(settings_file, 'w') as outfile:
         for line in lines:
             outfile.write(line)
 
 
-def compose(s, string, value):
-    key = '{{' + string + '}}'
-    if key in s:
-        s.replace(key, value)
-    return s
+def compose(string, key, value):
+    """
+    Takes string and replaces the template key with value
+    :param string:
+    :param key:
+    :param value:
+    :return String:
+    """
+    key = '{{' + key + '}}'
+    if key in string:
+        string.replace(key, value)
+    return string
 
 
 def bot_main():
-    oauth_helper = PrawOAuth2Mini(reddit_client, app_key=settings.reddit_app_key, app_secret=settings.reddit_app_secret,
-                                  access_token=settings.reddit_access_token, scopes=settings.scopes,
+    """
+    Main function of the bot. Contains logic loop and oauth
+    """
+    oauth_helper = PrawOAuth2Mini(REDDIT_CLIENT,
+                                  app_key=settings.reddit_app_key,
+                                  app_secret=settings.reddit_app_secret,
+                                  access_token=settings.reddit_access_token,
+                                  scopes=settings.scopes,
                                   refresh_token=settings.reddit_refresh_token)
     print 'UsernameBot Online!'
     while True:
@@ -57,16 +84,25 @@ def bot_main():
 
 
 def bot_logic(oauth_helper):
+    """
+    Logic function of the bot gets new submissions and possesses them
+    :param oauth_helper:
+    """
     oauth_helper.refresh()
-    subreddit = reddit_client.get_subreddit(settings.subreddit)
+    subreddit = REDDIT_CLIENT.get_subreddit(settings.subreddit)
     submissions = subreddit.get_new(limit=100)
     for submission in submissions:
         if isinstance(submission, praw.objects.Submission):
-            if submission.id not in already_done:
+            if submission.id not in ALREADY_DONE:
                 bot_process_post(subreddit, submission)
 
 
 def bot_process_post(subreddit, submission):
+    """
+    Submissions that are not already done are possessed here
+    :param subreddit:
+    :param submission:
+    """
     username = submission.title
     print 'Possessing %s (ID:%s):' % (username, submission.id)
     if False:  # submission.get_sticky():
@@ -77,12 +113,17 @@ def bot_process_post(subreddit, submission):
         else:
             check_username_posts(subreddit, submission)
     print '-- done!\n'
-    already_done.add(submission.id)
+    ALREADY_DONE.add(submission.id)
 
 
 def check_username_posts(subreddit, submission):
+    """
+    Submissions that are not skipped are then checked and dealt with accordingly
+    :param subreddit:
+    :param submission:
+    """
     username = submission.title
-    if not username_manager.valid_username(username):
+    if not USERNAME_MANAGER.valid_username(username):
         if len(username) > 20:
             print '-- removed due to name length'
         else:
@@ -90,11 +131,11 @@ def check_username_posts(subreddit, submission):
 
         submission.remove(False)
     else:
-        if username_manager.user_exists(username):
+        if USERNAME_MANAGER.user_exists(username):
             print '-- tagged as taken'
-            reddit_client.set_flair(subreddit, submission, flair_text='TAKEN')
+            REDDIT_CLIENT.set_flair(subreddit, submission, flair_text='TAKEN')
 
-
+# Main Function
 if __name__ == '__main__':
     if settings.reddit_access_token == '{{access_token}}':
         bot_init()
--- UsernameManager.py
+++ UsernameManager.py
@@ -1,16 +1,28 @@
-__author__ = 'Lachlan'
+#!/usr/bin/env python2
+"""
+Performs various operations on reddit usernames
+"""
 
 from praw import errors
 import re
 
 
-class UsernameManager:
-    user_pattern = re.compile("\A([0-9a-zA-Z])+\Z")
+class UsernameManager(object):
+    """
+    Performs various operations on reddit usernames
+    :param reddit_client:
+    """
+    user_pattern = re.compile(r"\A([0-9a-zA-Z])+\Z")
 
     def __init__(self, reddit_client):
         self.reddit_client = reddit_client
 
     def user_exists(self, username):
+        """
+        Checks if the user exists
+        :param username:
+        :return user exists:
+        """
         try:
             self.reddit_client.get_redditor(username, fetch=True)
             return True
@@ -18,6 +30,11 @@ class UsernameManager:
             return False
 
     def valid_username(self, username):
+        """
+        Checks if the username is valid
+        :param username:
+        :return valid:
+        """
         if len(username) < 3 or len(username) > 20:
             return False
         if not re.match(self.user_pattern, username):
